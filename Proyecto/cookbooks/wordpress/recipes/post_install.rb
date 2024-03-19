d4058696356cb80faa6ee4c04f17a5724ec51f95
amddi <amddi.comercial@gmail.com>
2024-03-19 00:34:10 -0500

    Se hiciero nmodificaciones para agregar el idioma y que la imagen se vea

--- Proyecto/cookbooks/wordpress/recipes/post_install.rb
+++ Proyecto/cookbooks/wordpress/recipes/post_install.rb
@@ -20,6 +20,27 @@ end
 
 # Instalar Wordpress y configurar
 execute 'Finish Wordpress installation' do
-  command 'sudo -u vagrant -i -- wp core install --path=/opt/wordpress/ --url=192.168.56.2 --title="EPNEWMAN - HAD - CHRISTIAN REYES SOTELO" --admin_user=admin --admin_password="Epnewman123" --admin_email=admin@epnewman.edu.pe'
-  not_if 'wp core is-installed', environment: { 'PATH' => '/bin:/usr/bin:/usr/local/bin' }
+  command 'sudo -u vagrant -i -- wp core install --path=/opt/wordpress/ --url=192.168.56.2 --title="EPNEWMAN - Herramientas de automatización de despliegues" --admin_user=admin --admin_password="Epnewman123" --admin_email=admin@epnewman.edu.pe'
+  not_if 'wp core is-installed --path=/opt/wordpress/', environment: { 'PATH' => '/bin:/usr/bin:/usr/local/bin' }
+end
+
+# Establecer permisos en el directorio wp-content
+directory '/opt/wordpress/wp-content' do
+  owner 'vagrant'
+  group 'vagrant'
+  mode '0755'
+  recursive true
+  action :create
+end
+
+# Descargar archivos de idioma en español
+execute 'Download Spanish language files' do
+  command 'sudo -u vagrant -i -- wp language core install es_ES --path=/opt/wordpress/'
+  not_if 'wp language core is-installed es_ES --path=/opt/wordpress/', environment: { 'PATH' => '/bin:/usr/bin:/usr/local/bin' }
+end
+
+# Establecer idioma por defecto en español
+execute 'Set Spanish as default language' do
+  command 'sudo -u vagrant -i -- wp site switch-language es_ES --path=/opt/wordpress/'
+  not_if 'wp site get --field=home --path=/opt/wordpress/', environment: { 'PATH' => '/bin:/usr/bin:/usr/local/bin' }
 end
